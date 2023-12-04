4945cf3af91f93f566f0e41a0f7c4aca032cfdd2
Kristoffer Troelsen <kristroelsen@hotmail.com>
2023-12-04 14:04:23 +0100

    sdas

--- Assets/Scripts/Kris/BallMovement.cs
+++ Assets/Scripts/Kris/BallMovement.cs
@@ -28,11 +28,6 @@ public class BallMovement : MonoBehaviour
             ballRB.AddForce(Vector3.forward * -ballSpeed, ForceMode.Impulse);
         }*/
 
-        /*if(transform.position.y < 1)
-        {
-            
-        }*/
-
         if(Input.GetKeyDown(KeyCode.Space))
         {
             ResetToStartPosition();
@@ -60,13 +55,42 @@ public class BallMovement : MonoBehaviour
     public void ResetToStartPosition()
     {
         transform.position = startPos;
+        ballRB.velocity = Vector3.zero;
+        ballRB.angularVelocity = Vector3.zero;
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("ClubHead"))
         {
+            // Get the impact point in world space
+            Vector3 colliderImpact = other.ClosestPointOnBounds(transform.position);
+
+            // Calculate the direction from the collider's center to the impact point
+            Vector3 directionImpact = (colliderImpact - other.transform.position).normalized;
+
+            // Calculate the angle between the collider's forward direction and the direction to the impact point
+            float angle = Vector3.Angle(other.transform.forward, directionImpact);
+
+            // Determine the axis of rotation from the collider's forward to the direction to the impact point
+            Vector3 rotationAxis = Vector3.Cross(other.transform.forward, directionImpact).normalized;
+
+            // Use the angle and axis to create a rotation quaternion
+            Quaternion rotation = Quaternion.AngleAxis(angle, rotationAxis);
+
+            // Get the rotated forward direction (side of the collider)
+            Vector3 colliderDirection = rotation * other.transform.forward;
+
+            // Adjust ballSpeed based on the velocity of the golf club's swing
+            float newBallSpeed = ballSpeed * other.GetComponent<Rigidbody>().velocity.magnitude;
+
+            // Add force in the direction of the side of the collider with adjusted speed
+            ballRB.AddForce(colliderDirection * newBallSpeed, ForceMode.Impulse);
+
             ballRB.AddForce(Vector3.up * ballLift, ForceMode.Impulse);
+
+
             //ballRB.AddForce(Vector3.forward * -ballSpeed, ForceMode.Impulse);
         }
     }
